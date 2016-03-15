01f720f56d3b7dc0ca5ba19bea491fe2b06b6d3d
IvanJijon <ivanjijon@hotmail.com>
2016-03-16 00:27:33 +0100

    add coordinates

---
+++ application/controllers/coordinates.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Coordinates extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('coordinates');
+	}
+
+}
+
+/* End of file coordinates.php */
+/* Location: ./application/controllers/coordinates.php */
