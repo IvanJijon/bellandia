eb0db6973a885383ed8f1d33e22b1cff4306caf0
acgav1090 <castro.alfredo00@gmail.com>
2016-03-20 19:55:01 +0100

    avancement CSS
    Corrections CSS + footer information

--- application/views/contact.php
+++ application/views/contact.php
@@ -15,10 +15,6 @@ include("inc/menu-scroll.php");
 	<h1 style="margin-bottom: 55px;">
 		CONTÁCTENOS
 	</h1>
-<!-- 	<iframe id="maps"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.6434393766463!2d-77.98323368512438!3d-1.3904925361412062!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d3e3b6f537fa95%3A0x742c57ab73fb069e!2sBellandia+Lodge!5e0!3m2!1sfr!2sfr!4v1458068009308" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
- -->
-
-
 	<div id="contact">
 		<div class="msg">
 			<?php if (isset($message_display)) { echo $message_display; } ?>
--- application/views/inc/home-footer.php
+++ application/views/inc/home-footer.php
@@ -10,6 +10,17 @@
 			<object data="<?php echo asset_url(); ?>img/icons/Twiitter_icon.svg" type="image/svg+xml"></object>
 		</div>
 	</div>
+	<div id="footer-content">
+		<div id="footer-title">
+			BELLANDIA LODGE
+			El Puyo - Ecuador
+		</div>
+		<div id="information"> 
+			<strong>Dirección :</strong> Km 21, Vía Puyo Km 6 Vía a Boayacu, Tena, Ecuador</br>
+			<strong>Teléfonos :</strong> +593996009781</br>
+			<strong>Mail :</strong> demo@bellandia.com
+		</div>
+	</div>
 </div>
 
 </body>
