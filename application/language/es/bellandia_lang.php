53d5c782a2057405923fe02c19015ee487993aff
IvanJijon <ivanjijon@hotmail.com>
2016-09-29 11:55:59 +0200

    Ignore database.php
    Creacion es/bellandia_lang.php

---
+++ application/language/es/bellandia_lang.php
@@ -0,0 +1,13 @@
+<?php 
+// MENU
+$lang['menu-rooms'] = "HABITACIONES";
+$lang['menu-activities'] = "ACTIVIDADES";
+$lang['menu-gallery'] = "GALERÍA";
+$lang['menu-contact'] = "CONTÁCTENOS";
+$lang['menu-coordinates'] = "CÓMO LLEGAR";
+$lang['menu-reserve'] = "RESERVAR";
+
+// HOME
+$lang['home-title'] = "Archives";
+
+?>
