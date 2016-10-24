4e3869c6ffeae0c821608e096840ce15ac2ea3a0
IvanJijon <ivanjijon@hotmail.com>
2016-10-24 19:36:38 +0200

    Add lang FR

---
+++ application/language/fr/bellandia_lang.php
@@ -0,0 +1,130 @@
+<?php 
+// MENU - HOME
+$lang['menu-rooms'] = "CHAMBRES";
+$lang['menu-activities'] = "ACTIVITES";
+$lang['menu-gallery'] = "GALLERIE";
+$lang['menu-contact'] = "CONTACTEZ NOUS";
+$lang['menu-coordinates'] = "COMMENT S'Y RENDRE";
+$lang['menu-reserve'] = "RESERVAR";
+
+// HOME
+$lang['home-video'] = "DECOUVREZ LA VIDEO";
+$lang['home-welcome'] = "BIENVENUE A";
+$lang['home-bellandia-caps'] = "BELLANDIA";
+$lang['home-bellandia-desc'] = "Bellandia est un endroit charmant dans l'Amazonie équatorienne. Un site unique qui dispose de 4 chambres et 2 chalets modernes uniques dans le pays, couvertes de végétation, bioclimatiques, qui donnent le sentiment de vivre immergé dans la nature. Sa grande chute d'eau, la pureté de sa piscine naturelle et les magnifiques sentiers en font l'endroit idéal pour se détendre, profiter, apprendre et s'amuser.";
+$lang['home-gal-img'] = "GALLERIE D'IMAGES";
+$lang['home-enjoy-rooms'] = "PROFITEZ DE NOS";
+$lang['home-rooms-caps'] = "CHALETS";
+
+$lang['home-doble-twin'] = "DOBLE OU TWIN";
+$lang['home-doble-twin-desc'] = "Accès pour handicapés. Dispose d'une chambre et une salle de bain. Grand lit ou lits jumeaux. Terrasse avec vue magnifique sur la fôret. Capacité maximale 2 personnes.";			
+
+$lang['home-dos-habs'] = "CHALET 2 CHAMBRES";
+$lang['home-dos-habs-desc'] = "Dispose de 2 chambres et une salle de bain. Une des chambres est équipée d'un grand lit et l'autre 2 lits jumeaux. Terrasse avec vue magnifique sur la fôret. Capacité maximale 4 personnes.";
+
+$lang['home-verde-amp'] = "CHALET VERT SPATIEUX";
+$lang['home-verde-amp-desc'] = "Technologie avancée, ecologique. Recouverte de terre et végétation, elle bénéficie de l'effet bioclimatique. Unique dans le pays. 2 grands lits, 1 canapé grand, TV. Terrasse avec vue magnifique sur la fôret. Capacité maximale 4 personnes";
+
+$lang['home-verde'] = "CHALET VERDE";
+$lang['home-verde-desc'] = "Technologie avancée, ecologique. Recouverte de terre et végétation, elle bénéficie de l'effet bioclimatique. Unique dans le pays. Eau chaude rechauffée grâce à l'énergie solaire. 1 grand lit et un lit pour une personne, TV. Terrasse avec vue magnifique sur la fôret. Capacité maximale 3 personnes.";
+
+$lang['home-mas-info'] = "Voir les détails";
+$lang['home-ver-disponibilidad'] = "VOIR DISPONIBILITE";
+
+// ROOMS
+$lang['rooms-rooms'] = "CHAMBRES";
+$lang['rooms-reserve'] = "RESERVATION";
+
+// ACTIVITIES
+$lang['activities-activities'] = "ACTIVITES";
+
+// CONTACT
+$lang['contact-contact'] = "CONTACTEZ NOUS";
+$lang['contact-name'] = "Prénom";
+$lang['contact-email'] = "Courriel";
+$lang['contact-msg'] = "Votre message";
+$lang['contact-msg-container'] = "Saisissez ici votre message...";
+$lang['contact-send'] = "Envoyer";
+
+// LOCATION (COORDINATES)
+$lang['coordinates-coordinates'] = "COMMENT S'Y RENDRE";
+$lang['coordinates-bellandia-lodge-caps'] = "BELLANDIA LODGE";
+$lang['coordinates-address'] = "Adresse : ";
+$lang['coordinates-address-desc'] = "Km 21 vía Puyo-Tena y 6 Km vía a Boayacu, Puyo, Ecuador";
+$lang['coordinates-phone'] = "Téléphones : ";
+$lang['coordinates-phone-desc'] = "+593983394528, +593998580191, +59322906987";
+$lang['coordinates-mail'] = "Courriel : ";
+$lang['coordinates-mail-desc'] = "info@bellandia.com";
+
+// RESERVE (BOOK)
+$lang['reserve-reserve'] = "RESERVER";
+$lang['reserve-txt-1'] = "Notre désir est de protégér l'écosystème particulier de Bellandia. Il est indispensable que les visites soient gérées avec précaution.";
+$lang['reserve-txt-2'] = "On vous prie de bien vouloir remplir ce formulaire de pré-réserve et on se mettra en contact avec vous.";
+$lang['reserve-price'] = "Prix des chambres";
+$lang['reserve-includes'] = "(Petit déjeuner et activités incluses)";
+$lang['reserve-1-guest'] = "1 personne ";
+$lang['reserve-2-guest'] = "2 personnes ";
+$lang['reserve-3-guest'] = "3 personnes ";
+$lang['reserve-4-guest'] = "4 personnes ";
+$lang['reserve-1-guest-price'] = "$80/nuit*";
+$lang['reserve-2-guest-price'] = "$140/nuit*";
+$lang['reserve-3-guest-price'] = "$189/nuit*";
+$lang['reserve-4-guest-price'] = "$210/nuit*";
+$lang['reserve-iva'] = "*Prix sans TVA.";
+$lang['reserve-entry-date'] = "Date d'entrée";
+$lang['reserve-departure-date'] = "Date de sortie";
+$lang['reserve-name'] = "Prénom";
+$lang['reserve-surname'] = "Nom";
+$lang['reserve-email'] = "Courriel";
+$lang['reserve-adults'] = "Nombre d'adultes";
+$lang['reserve-children'] = "Nombre d'enfants";
+$lang['reserve-msg'] = "Précisions";
+$lang['reserve-msg-container'] = "Saisissez ici votre message.";
+$lang['reserve-send'] = "ENVOYER";
+
+// RESERVE (BOOK) - SUCCESS
+$lang['reserve-reserve'] = "RESERVER";
+$lang['reserve-txt-1'] = "Notre désir est de protégér l'écosystème particulier de Bellandia. Il est indispensable que les visites soient gérées avec précaution.";
+$lang['reserve-txt-2'] = "On vous prie de bien vouloir remplir ce formulaire de pré-réserve et on se mettra en contact avec vous.";
+$lang['reserve-price'] = "Prix des chambres";
+$lang['reserve-includes'] = "(Petit déjeuner et activités incluses)";
+$lang['reserve-1-guest'] = "1 personne ";
+$lang['reserve-2-guest'] = "2 personnes ";
+$lang['reserve-3-guest'] = "3 personnes ";
+$lang['reserve-4-guest'] = "4 personnes ";
+$lang['reserve-1-guest-price'] = "$80/nuit*";
+$lang['reserve-2-guest-price'] = "$140/nuit*";
+$lang['reserve-3-guest-price'] = "$189/nuit*";
+$lang['reserve-4-guest-price'] = "$210/nuit*";
+$lang['reserve-iva'] = "*Prix sans TVA.";
+$lang['reserve-entry-date'] = "Date d'entrée";
+$lang['reserve-departure-date'] = "Date de sortie";
+$lang['reserve-name'] = "Prénom";
+$lang['reserve-surname'] = "Nom";
+$lang['reserve-email'] = "Courriel";
+$lang['reserve-adults'] = "Nombre d'adultes";
+$lang['reserve-children'] = "Nombre d'enfants";
+$lang['reserve-msg'] = "Précisions";
+$lang['reserve-msg-container'] = "Saisissez ici votre message.";
+$lang['reserve-send'] = "ENVOYER";
+
+// HOME-FOOTER
+$lang['home-footer-connect'] = "SE CONNECTER AVEC NOUS";
+$lang['home-footer-follow'] = "SUIVEZ NOUS";
+$lang['home-footer-bellandia-lodge-caps'] = "BELLANDIA LODGE";
+$lang['home-footer-address'] = "Adresse : ";
+$lang['home-footer-address-desc'] = "Km 21 vía Puyo-Tena y 6 Km vía a Boayacu, Puyo, Ecuador";
+$lang['home-footer-phone'] = "Téléphones : ";
+$lang['home-footer-phone-desc'] = "+593983394528, +593998580191, +59322906987";
+$lang['home-footer-mail'] = "Courriel : ";
+$lang['home-footer-mail-desc'] = "info@bellandia.com";
+
+// MENU-SCROLL
+$lang['menu-scroll-home'] = "ACCUEIL";
+$lang['menu-scroll-rooms'] = "CHAMBRES";
+$lang['menu-scroll-activities'] = "ACTIVITES";
+$lang['menu-scroll-gallery'] = "CALLERIE";
+$lang['menu-scroll-contact'] = "CONTACTEZ NOUS";
+$lang['menu-scroll-coordinates'] = "COMMENT S'Y RENDRE"; 
+$lang['menu-scroll-reserve'] = "RESERVER";
+?>
